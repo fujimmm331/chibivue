ba84b6d67cb14c36479f87cbf3381d2284d79571
fujimmm331 <nyappy15th@gmail.com>
2025-02-04 22:08:39 +0900

    feat: event&attribute

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -2,7 +2,18 @@ import { createApp, h } from "chibivue";
 
 const app = createApp({
   render() {
-    return h('div', {}, [h('button', {}, ['Hello world.'])])
+    return h('div', {}, [
+      h('p', { style: 'color: red; font-weight: bold;' }, ['Hello world.']),
+      h(
+        'button',
+        {
+          onClick() {
+            alert('Hello world!')
+          },
+        },
+        ['click me!'],
+      ),
+    ])
   },
 })
 
--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -10,7 +10,9 @@ export interface RendererOptions<
 
   setElementText(node: HostNode, text: string): void
 
-  insert(child: HostNode, parent: HostNode, anchor?: HostNode | null): void 
+  insert(child: HostNode, parent: HostNode, anchor?: HostNode | null): void
+
+  patchProp(el: HostElement, key: string, value: any): void;
 }
 
 export interface RendererNode {
@@ -26,6 +28,7 @@ export type RootRenderFunction<HostElement = RendererElement> = (
 
 export function createRenderer(options: RendererOptions) {
   const {
+    patchProp: hostPatchProp,
     createElement: hostCreateElement,
     createText: hostCreateText,
     insert: hostInsert,
@@ -34,6 +37,9 @@ export function createRenderer(options: RendererOptions) {
   function renderVNode(vnode: VNode | string) {
     if (typeof vnode === 'string') return hostCreateText(vnode)
     const el = hostCreateElement(vnode.type)
+    Object.entries(vnode.props).forEach(([key, value]) => {
+      hostPatchProp(el, key, value);
+    });
 
     for (const child of vnode.children) {
       const childEl = renderVNode(child)
